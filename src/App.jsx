de49e2eca48050bf3f5521f5024e240ed4798472
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-01-03 17:55:42 +0530

    Add a Note container is completed with styling of it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 function App() {
   return (
     <>
-      <div>
-        App
+      <div className="bg-blue-100 h-screen w-full">
+        <div className="px-14 py-10">
+            <form className="bg-blue-50 flex flex-col items-start p-7 rounded-md">
+              <h1 className="text-blue-950 font-semibold text-3xl">Add a Notes</h1>
+              <input
+                className="text-blue-950 w-[99%] font-medium text-xl border-none outline-none mt-3 mb-5 px-1 py-1" 
+                type="text" 
+                placeholder="Title"
+                id="note-title"
+              />
+              <textarea 
+                className="text-blue-950 w-[99%] border-none outline-none resize-none px-1 py-1"
+                placeholder="Take a note..." 
+                id="note-details"
+              />
+            </form>
+        </div>
+        <div></div>
       </div>
     </>
   )
