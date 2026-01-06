8332bb075807af4e9e032d2915f480e95e95901e
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-01-06 18:24:24 +0530

    Shadow is added to the form and button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className="bg-blue-100 h-screen w-full">
         <div className="px-14 py-10">
-            <form className="bg-blue-50 flex flex-col items-start p-7 rounded-md">
+            <form className="bg-blue-50 flex flex-col items-start p-7 rounded-md shadow-md">
               <h1 className="text-blue-950 font-semibold text-3xl">Add a Notes</h1>
               <input
                 className="text-blue-950 w-[99%] font-medium text-xl border-none outline-none mt-3 mb-5 px-1 py-1" 
@@ -16,7 +16,7 @@ function App() {
                 placeholder="Take a note..." 
                 id="note-details"
               />
-              <button className="bg-blue-950 text-white font-medium px-9 py-3 rounded-md cursor-pointer ml-auto mt-2">Add</button>
+              <button className="bg-blue-950 shadow-md text-white font-medium px-9 py-3 rounded-md cursor-pointer ml-auto mt-2">Add</button>
             </form>
         </div>
         <div></div>
