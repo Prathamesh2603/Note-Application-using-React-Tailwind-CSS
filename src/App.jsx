548e8018b8202020a6ec9fa5b475d27ac83da25b
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-01-10 00:38:14 +0530

    list design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { NotebookText } from 'lucide-react';
+import { NotebookText, Trash2 } from 'lucide-react';
 
 function App() {
   return (
     <>
       <div className="bg-blue-100 h-screen w-full">
-        <div className="px-14 py-10">
+        <div className="px-14 py-8">
             <form className="bg-blue-50 flex flex-col items-start p-7 rounded-md shadow-md">
               <h1 className="text-blue-950 font-semibold text-3xl">Add a Notes</h1>
               <input
@@ -21,12 +21,22 @@ function App() {
               <button className="bg-blue-950 shadow-md text-white font-medium px-9 py-3 rounded-md cursor-pointer ml-auto mt-2">Add</button>
             </form>
         </div>
-        <div>
-          <div className='flex items-center px-14 gap-2'>
+        <div className='px-14 h-1/2'>
+          <div className='flex items-center gap-3.5'>
             <NotebookText color='#172554'/>
-            <h2 className='text-blue-950 font-medium text-2xl'>My Notes</h2>
+            <h2 className='text-blue-950 font-medium text-xl'>My Notes</h2>
           </div>
-          <div></div>
+          <div className='h-[80%] flex items-center gap-6'>
+            <div className='bg-white h-[80%] w-[22%] rounded-md p-3 shadow-lg'>
+              <div className='flex justify-between items-center mb-2'>
+                <p className='text-blue-950 font-semibold text-2xl'>Feedback</p>
+                <Trash2 color='#172554'/>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores, incidunt dolor perspiciatis reiciendis debitis.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
