c352baa38a0c25eeee618d8c5fe70f9705d010a2
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-01-10 13:00:10 +0530

    Form submit adtion is handled and input value is handle with state variable(Two way binding)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { NotebookText, Trash2 } from 'lucide-react';
+import { useState } from 'react';
 
 function App() {
+  const [inputVal, setInputVal] = useState("");
+  
+  const hnndleChange = (e) => {
+    setInputVal(e.target.value);
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert(inputVal);
+    setInputVal("");
+  }
+
   return (
     <>
       <div className="bg-blue-100 h-screen w-full">
         <div className="px-14 py-8">
-            <form className="bg-blue-50 flex flex-col items-start p-7 rounded-md shadow-md">
+            <form onSubmit={handleSubmit} className="bg-blue-50 flex flex-col items-start p-7 rounded-md shadow-md">
               <h1 className="text-blue-950 font-semibold text-3xl">Add a Notes</h1>
               <input
                 className="text-blue-950 w-[99%] font-medium text-xl border-none outline-none mt-3 mb-5 px-1 py-1" 
                 type="text" 
                 placeholder="Title"
                 id="note-title"
+                onChange={hnndleChange}
+                value={inputVal}
               />
               <textarea 
                 className="text-blue-950 w-[99%] border-none outline-none resize-none px-1 py-1"
@@ -26,11 +42,11 @@ function App() {
             <NotebookText color='#172554'/>
             <h2 className='text-blue-950 font-medium text-xl'>My Notes</h2>
           </div>
-          <div className='h-[80%] flex items-center gap-6'>
-            <div className='bg-white h-[80%] w-[22%] rounded-md p-3 shadow-lg'>
+          <div id='list-container' className='h-[80%] flex items-center gap-6 overflow-x-auto'>
+            <div className='bg-white h-[80%] w-65 rounded-md p-3 shadow-lg shrink-0'>
               <div className='flex justify-between items-center mb-2'>
                 <p className='text-blue-950 font-semibold text-2xl'>Feedback</p>
-                <Trash2 color='#172554'/>
+                <span><Trash2 color='#172554'/></span>
               </div>
               <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores, incidunt dolor perspiciatis reiciendis debitis.</p>
