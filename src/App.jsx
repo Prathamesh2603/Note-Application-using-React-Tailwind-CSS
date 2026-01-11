0d5aaa1bb282877642ae1848cf6da966153251c6
Prathamesh2603 <prathameshpawar645@gmail.com>
2026-01-11 16:11:34 +0530

    Did some clean-up and fromating. Creating new div of data in MY Notes with data enter in input and textarea

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ import { useState } from 'react';
 function App() {
   const [inputVal, setInputVal] = useState("");
   const [textAVal, setTextAVal] = useState("");
-  
-  const handleChange = (e) => {
-    setInputVal(e.target.value);
-  }
+  const [task, setTask] = useState([]);
 
-  const handleChange2 = (e) => {
-    setTextAVal(e.target.value);
-
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    alert(inputVal);
+    const copyTask = [...task];
+    copyTask.push({title: inputVal, note: textAVal});
+    
+    setTask(copyTask)
+    
     setInputVal("");
     setTextAVal("");
   }
@@ -26,46 +23,61 @@ function App() {
     <>
       <div className="bg-blue-100 h-screen w-full">
         <div className="px-14 py-8">
-            <form onSubmit={handleSubmit} className="bg-blue-50 flex flex-col items-start p-7 rounded-md shadow-md">
-              <h1 className="text-blue-950 font-semibold text-3xl">Add a Notes</h1>
-              <input
-                className="text-blue-950 w-[99%] font-medium text-xl border-none outline-none mt-3 mb-5 px-1 py-1" 
-                type="text" 
-                placeholder="Title"
-                id="note-title"
-                onChange={handleChange}
-                value={inputVal}
-              />
-              <textarea 
-                className="text-blue-950 w-[99%] border-none outline-none resize-none px-1 py-1"
-                placeholder="Take a note..." 
-                id="note-details"
-                onChange={handleChange2}
-                value={textAVal}
-              />
-              <button className="bg-blue-950 shadow-md text-white font-medium px-9 py-3 rounded-md cursor-pointer ml-auto mt-2">Add</button>
-            </form>
+          <form
+            onSubmit={handleSubmit}
+            className="bg-blue-50 flex flex-col items-start p-7 rounded-md shadow-md"
+          >
+            <h1 className="text-blue-950 font-semibold text-3xl">
+              Add a Notes
+            </h1>
+            <input
+              className="text-blue-950 w-[99%] font-medium text-xl border-none outline-none mt-3 mb-5 px-1 py-1"
+              type="text"
+              placeholder="Title"
+              id="note-title"
+              onChange={(e) => {
+                setInputVal(e.target.value);
+              }}
+              value={inputVal}
+            />
+            <textarea
+              className="text-blue-950 w-[99%] border-none outline-none resize-none px-1 py-1"
+              placeholder="Take a note..."
+              id="note-details"
+              onChange={(e) => {
+                setTextAVal(e.target.value);
+              }}
+              value={textAVal}
+            />
+            <button className="bg-blue-950 shadow-md text-white font-medium px-9 py-3 rounded-md cursor-pointer ml-auto mt-2">
+              Add
+            </button>
+          </form>
         </div>
-        <div className='px-14 h-1/2'>
-          <div className='flex items-center gap-3.5'>
-            <NotebookText color='#172554'/>
-            <h2 className='text-blue-950 font-medium text-xl'>My Notes</h2>
+        <div className="px-14 h-1/2">
+          <div className="flex items-center gap-3.5">
+            <NotebookText color="#172554" />
+            <h2 className="text-blue-950 font-medium text-xl">My Notes</h2>
           </div>
-          <div id='list-container' className='h-[80%] flex items-center gap-6 overflow-x-auto'>
-            <div className='bg-white h-[80%] w-65 rounded-md p-3 shadow-lg shrink-0'>
-              <div className='flex justify-between items-center mb-2'>
-                <p className='text-blue-950 font-semibold text-2xl'>Feedback</p>
-                <span><Trash2 color='#172554'/></span>
-              </div>
-              <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus asperiores, incidunt dolor perspiciatis reiciendis debitis.</p>
-              </div>
-            </div>
+          <div id="list-container" className="h-[80%] flex items-center gap-6 overflow-x-auto">
+            {task.map((elem, idx) => {          
+              return (
+                  <div key={idx} className="bg-white h-[80%] w-68 rounded-md p-4 shadow-lg shrink-0">
+                    <div className="flex justify-between items-center mb-2">
+                      <p className="text-blue-950 font-semibold text-2xl">{elem.title}</p>
+                      <button><Trash2 color="#172554" /></button>
+                    </div>
+                    <div>
+                      <p>{elem.note}</p>
+                    </div>
+                  </div>
+                )
+            })}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App
